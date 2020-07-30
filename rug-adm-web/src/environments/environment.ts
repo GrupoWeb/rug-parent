d58ee2bbe43dbb2d50ec8b5388033a47ac328824
Juan Jose Jolon <jjolong@miumg.edu.gt>
2020-07-30 09:55:54 -0600

    env

--- rug-adm-web/src/environments/environment.ts
+++ rug-adm-web/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   api_url: 'http://128.5.9.19/rug-adm/rs',
   base_url: 'http://128.5.9.19/rug-adm',
-  pentaho_url: 'http://128.5.9.5:28080/pentaho'
+  pentaho_url: 'http://localhost:8080/pentaho'
 };
 // export const environment = {
 //   production: false,
